9da4a5884f6b626c3304497a56aafc7caf53490c
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2025-10-23 17:06:45 -0300

    Adicionei os cards dos produtos e uma barra de pesquisa(somente visual)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import './assets/css/App.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Img from "./assets/img/desconto.png";
+import "./assets/css/App.css";
+
+const produtos = [
+  { id: 1, nome: "A", valor: 10.99, estrelas: 5 },
+  { id: 2, nome: "A", valor: 10.99, estrelas: 5 },
+  { id: 3, nome: "A", valor: 10.99, estrelas: 5 },
+].map((s) => ({
+  ...s,
+}));
 
 function App() {
   return (
     <>
       <Navbar />
       <div>
-        
+        <div className="espaco">
+          <input type="text" placeholder="Pesquisar pedidos..." />
+        </div>
+        <section className="section">
+          <div className="itens-grid">
+            {produtos.map((s) => (
+              <div className="card">
+                <div className="card-body">
+                  <img src={Img} alt="produto1" width="100px" />
+                  <p>{s.nome}</p>
+                  <p>{s.estrelas}</p>
+                  <p>{s.valor}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
