408836f90b78d583e846ab3231aa3e183916eada
Matheus-Spirit <matheusdiasmendespop@gmail.com>
2025-10-24 19:47:58 -0300

    Pagina de carrinho mostrando as informações adicionadas nele

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,39 @@
+import { useState, useEffect } from "react";
+import "../assets/css/Cart.css"
+
 function Cart() {
-    return(
-        <>
-        
-        </>
-    );
+  const [carts, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/cart")
+      .then((res) => res.json())
+      .then((data) => setCart(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const totPrice = carts.reduce((tot, item) => {
+    return tot + item.product.price * item.quantity;
+  }, 0);
+
+  return (
+    <div className="container-cart">
+      <div>
+        {carts.map((s) => (
+          <div key={s.id}>
+            <p>ID do item: {s.id}</p>
+            <p>Produto: {s.product.name}</p>
+            <p>Quantidade: {s.quantity}</p>
+            <p>Preço: R$ {s.product.price}</p>
+            <p>Subtotal: R$ {s.product.price * s.quantity}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Preço Total: R$ {totPrice.toFixed(2)}</h2>
+        <button>Confirmar Compra</button>
+      </div>
+    </div>
+  );
 }
+
 export default Cart;
